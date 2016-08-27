eb796b905af9048cd608ca8a692f432cb6df6f5c
Ian Fitzpatrick <itfitzpatrick@gmail.com>
2016-08-26 17:29:24 -0700

    Make certain events configurable from host application

--- the-graph/the-graph-graph.js
+++ the-graph/the-graph-graph.js
@@ -115,14 +115,28 @@
       ReactDOM.findDOMNode(this).addEventListener("the-graph-node-remove", this.removeNode);
 
       var appDomNode = ReactDOM.findDOMNode(this.props.app);
-      appDomNode.addEventListener("mousedown", this.startMarqueeSelect);
-      window.addEventListener("mouseup", this.stopMarqueeSelect);
-      appDomNode.addEventListener("mousemove", this.moveMarqueeSelect);
+      appDomNode.addEventListener(
+        this.getEvent('marqueeSelectStartEvent'), this.startMarqueeSelect);
+    },
+    defaultEventConfig: {
+      marqueeSelectStartEvent: 'mousedown',
+      marqueeSelectEndEvent: 'mouseup',
+      marqueeSelectEvent: 'mousemove',
+      marqueeSelectFilter: function (event) {return event.button === 1}
+    },
+    getEvent: function (key) {
+      return this.props.eventConfig[key] || this.defaultEventConfig[key];
     },
     startMarqueeSelect: function (event) {
-      if (event.button !== 1) {
+      if (!this.getEvent('marqueeSelectFilter')(event)) {
         return;
       }
+
+      var appDomNode = ReactDOM.findDOMNode(this.props.app);
+      window.addEventListener(
+        this.getEvent('marqueeSelectEndEvent'), this.stopMarqueeSelect);
+      appDomNode.addEventListener(
+        this.getEvent('marqueeSelectEvent'), this.moveMarqueeSelect);
 
       var appX = this.props.app.state.x;
       var appY = this.props.app.state.y;
@@ -201,10 +215,9 @@
       };
     },
     stopMarqueeSelect: function (event) {
-      if (event.button !== 1) {
+      if (!this.getEvent('marqueeSelectFilter')(event)) {
         return;
       }
-
       this.calculateMarqueeSelect(event);
 
       this.setState({
@@ -215,6 +228,14 @@
         marqueeSelectCurrentY: null
       });
       this.markDirty();
+
+      var appDomNode = ReactDOM.findDOMNode(this.props.app);
+      window.removeEventListener(
+        this.getEvent('marqueeSelectEndEvent'), this.stopMarqueeSelect);
+      appDomNode.removeEventListener(
+        this.getEvent('marqueeSelectEvent'), this.moveMarqueeSelect);
+
+
     },
     edgePreview: null,
     edgeStart: function (event) {
